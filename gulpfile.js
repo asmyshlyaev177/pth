ffa6799da13cd0263ebe7bfd74e2b8605cee4d79
Alex <asmyshlyaev177@gmail.com>
2018-03-08 20:21:31 +0300

    gulp and autoprefixer

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var sourcemaps = require('gulp-sourcemaps');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('watch', function() {
+  return gulp.src('./css/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(watch('./css/**/*.scss'))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer("last 5 version", "> 2%"))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('build', function() {
+  return gulp.src('./css/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer("last 5 version", "> 2%"))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./css'));
+});
