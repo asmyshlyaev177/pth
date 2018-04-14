39411e95612a9f1e9b1894ad537e8f2d7f26b3c1
Alex <asmyshlyaev177@gmail.com>
2018-04-14 17:54:42 +0300

    gulpfile fix and vision block

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,15 @@ var watch = require('gulp-watch');
 var sourcemaps = require('gulp-sourcemaps');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
 
 gulp.task('watch', function() {
   return gulp.src('./css/**/*.scss')
+    .pipe(plumber())
     .pipe(watch('./css/**/*.scss'))
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}))
+    // .on('error', swallowError)
     .pipe(autoprefixer("last 5 version", "> 2%"))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./css'));
